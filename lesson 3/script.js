8eddc860fa2e4a1ae719924e5a0b1a1f1c8cc830
iitzkushal <iitzkushal@gmail.com>
2026-08-07 21:02:16 +0545

    Lesson 3: Data Types

---
+++ lesson 3/script.js
@@ -0,0 +1,22 @@
+let name = "kushal";
+let age =18;
+let isStudent = true;
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+
+let firstName = "Kushal";
+let favoriteGame = "GTA V";
+let age = 18;
+let isLearning = true;
+
+console.log(firstName);
+console.log(favoriteGame);
+console.log(age);
+console.log(isLearning);
+
+console.log(typeof firstName);
+console.log(typeof favoriteGame);
+console.log(typeof age);
+console.log(typeof isLearning);
