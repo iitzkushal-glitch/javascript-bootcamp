d9904db886e06b46c20df684912f6a767b32d60c
iitzkushal <iitzkushal@gmail.com>
2026-08-07 20:51:26 +0545

    lesson 2 : variables

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -1,1 +1,5 @@
 console.log("hello world!")
+let name = "john pork"
+let age = 30
+let favoritegame = "minecraft"
+
---
+++ lesson 2/scropt.js
@@ -0,0 +1,10 @@
+let name ="kushal name";
+let age = 18;
+let favoritegame = "RDR2";
+
+console.log(name);
+console.log(age);
+console.log(favoritegame);
+
+favoritegame = "GTA V"
+console.log(favoritegame);
