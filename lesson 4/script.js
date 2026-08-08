870b15436601be614fd1a45258f9e91ee89d94f5
iitzkushal <iitzkushal@gmail.com>
2026-08-08 09:52:06 +0545

    lesson 4: operations

---
+++ lesson 4/script.js
@@ -0,0 +1,38 @@
+let a =15;
+let b =4;
+
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+
+let score = 10;
+score = 20;
+
+score = score + 2;// insted of writing this i can do this;
+
+score += 5;// this will add 5 to score same goes for other 
+//score -= 5;
+//score *= 5;
+//score /= 5;
+//scpre %= 5;
+
+
+
+console.log (score);
+
+18 == "18" // '==' this checks wherher the value is equal while allowing java script to convert types
+18 === "18"// '===' this will give reasult false because is is string and another is number
+10 != 5 // != this simply means not equal 
+
+let money = 100;
+
+money += 50;
+money -= 30;
+money *= 2;
+console.log(money)
+console.log(100>20);
+console.log(20>100);
+console.log(10 !==10);
+
