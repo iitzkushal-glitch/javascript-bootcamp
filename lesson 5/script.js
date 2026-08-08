137ddc086e57d2071f8dffff3f5bc6e55d88e3b3
iitzkushal <iitzkushal@gmail.com>
2026-08-08 16:46:13 +0545

    lesson 5: if/else

---
+++ lesson 5/script.js
@@ -0,0 +1,39 @@
+let age1 =20;
+if (age1 >= 18){
+    console.log("you can enter");
+}else{
+    console.log("you are not old enough");
+}
+
+let age2 =10;
+if (age2 >= 18){
+    console.log("you can enter");
+}else{
+    console.log("you are not old enough");
+}
+
+let game = "RDR2";
+if (game ==="RDR2"){
+    console.log("that's my game");
+} else{
+    console.log("haha lol thats not my game")
+}
+
+let marks = 99;
+
+if (marks >=90) {
+    console.log("excellent");
+}else if(marks >= 80){
+    console.log("very good");
+}else if(marks >= 70){
+    console.log("good");
+}else{
+    console.log("need improvment");
+}
+
+let marks1 =33;
+if (marks1 >= 40){
+    console.log("pass")
+}else{
+console.log("fail")
+}
