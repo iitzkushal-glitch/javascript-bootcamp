c4dd579ca7723548c4c9f3bcee3655f08fcf2143
iitzkushal <iitzkushal@gmail.com>
2026-08-09 09:21:34 +0545

    mistake on code were corrected and added odd even and multiples

--- lesson 6/script.js
+++ lesson 6/script.js
@@ -10,7 +10,7 @@ for (let i=1; i<=5;i++){
 
 console.log("5-1 reverse counting loop");
 
-for (let i1 = 10; i1 <=1 ; i1-- ){
+for (let i1 = 10; i1 >= 1; i1--) {
     console.log(i1);
 }
 
@@ -20,7 +20,19 @@ for (let i2 = 1; i2 <=10; i2++){
 console.log(i2);
 }
 
-console.log("this loop is statrted from 2 and it will keep adding 2 until i3 >= 20 is true");
-for ( let i3 = 2; i3 >= 20; i3++ ){
+console.log("this loop is statrted from 2 and it will keep adding 2 until i3 <= 20 is true");
+
+for ( let i3 = 2; i3 <= 20; i3 += 2 ){
 console.log(i3);
+}
+
+console.log("odd number up to 15");
+
+for ( let i4=1 ; i4 <= 15; i4 += 2){
+    console.log( i4) ;
+}
+console.log("multiplaction of 5")
+
+for ( let i5=5; i5<=50; i5 += 5){
+console.log(i5)
 }
