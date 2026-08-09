be43d686f5e31a5d6523f4a78d9cdb87ae44dea9
iitzkushal <iitzkushal@gmail.com>
2026-08-09 11:04:37 +0545

    while loops

--- lesson 6/while loop/script.js
+++ lesson 6/while loop/script.js
@@ -1,3 +1,48 @@
 //   while loop format = while (consition){
 //      repit
 //   }
+ console.log("using while loop to count in revers");
+let i=10 ;
+
+while ( i >= 1 ){
+console.log(i);
+i--;
+}
+console.log("using while loop to add 2 number until i2 <= 20");
+let i2 = 2;
+
+while( i2 <= 20){
+console.log(i2);
+i2 += 2;
+}
+
+console.log("multiples of 5");
+let i3 = 5;
+
+while (i3 <= 50){
+console.log(i3);
+i3 += 5;
+}
+
+console.log("multiples of 5 ");
+let i4 = 1;
+while(i4 <= 10){
+    console.log(i4*5);
+    i4 ++;
+}
+console.log("odd number from 1-15");
+let i5 = 1;
+while( i5 <= 15){
+    console.log(i5);
+    i5 +=2;
+}
+
+let i6 = 3;
+while( i6 <= 30 ){
+    console.log(i6);
+    i6 +=3;
+}
+
+
+
+
