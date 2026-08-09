4dbb01032c1802025dea7c3bbf8b423741df52a1
iitzkushal <iitzkushal@gmail.com>
2026-08-09 09:04:18 +0545

    lesson 6 for loop

---
+++ lesson 6/script.js
@@ -0,0 +1,26 @@
+//  for loop ko format = for (starting point;condition;change){ 
+//                       code to repeat
+//                       }
+
+console.log("1-5 counting loop");
+
+for (let i=1; i<=5;i++){
+    console.log(i);
+}
+
+console.log("5-1 reverse counting loop");
+
+for (let i1 = 10; i1 <=1 ; i1-- ){
+    console.log(i1);
+}
+
+console.log("1-10 counting loop");
+
+for (let i2 = 1; i2 <=10; i2++){
+console.log(i2);
+}
+
+console.log("this loop is statrted from 2 and it will keep adding 2 until i3 >= 20 is true");
+for ( let i3 = 2; i3 >= 20; i3++ ){
+console.log(i3);
+}
