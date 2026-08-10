8e5b40688077eb0c528bcee2ac668e974eb73a1b
iitzkushal <iitzkushal@gmail.com>
2026-08-10 18:22:31 +0545

    parameter and calling function

--- lession 7/script.js
+++ lession 7/script.js
@@ -1,13 +1,32 @@
-function sayhello(){
+function sayHello(){
     console.log("hello guys");
 }
-
-sayhello();
+sayHello();
 
 
 
 function greet(){
     console.log("welcome to javascript");
 }
-
 greet();
+
+
+
+function greeting(name){
+    console.log( "hello" + name);
+}
+greeting("kushal");
+
+
+
+function getHim(name){
+    console.log("hello "+name);
+}
+getHim("kushal");
+getHim("thapa");
+
+function introduce(name, age){
+    console.log("my name is "+ name +"and i am " + age + "years old");
+}
+introduce("kushal",18);
+
