9b98bf2e296f56a7a029c35983b835be210d897b
iitzkushal <iitzkushal@gmail.com>
2026-08-10 19:00:42 +0545

    corrected file name and return function was added to lesson

--- lesson 7/script.js
+++ lesson 7/script.js
@@ -30,3 +30,30 @@ function introduce(name, age){
 }
 introduce("kushal",18);
 
+function calculateTotal(price, quantity){
+    let total = price * quantity;
+    console.log("total is " + total  );
+    
+}
+calculateTotal(978, 55);
+
+
+function sum(a, b){
+    console.log(a + b);
+}
+sum(22, 33);
+
+
+function addNumber(a ,b){
+    return a + b;
+}
+ let result = addNumber(10 ,10);
+ console.log(result);
+
+
+ function square(num){
+    return num * num;
+ }
+ let squares = square(67);
+console.log(squares);
+
