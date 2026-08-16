aa4da898fa851aa6d10cf03531a679865d0036ec
iitzkushal <iitzkushal@gmail.com>
2026-08-16 09:48:51 +0545

    more uses of function

--- practices/functions1/script.js
+++ practices/functions1/script.js
@@ -14,3 +14,21 @@ function namaste(){
 var greetings = namaste();
 console.log(greetings);
 console.log(namaste());
+
+function getUserRole(name,role){
+    switch (role){
+        case "admin":
+        return`${name}is admin with all access`    
+            break;
+        case "subadmin":
+            return`${name} is sub-admin with  access to create and delete cources`    
+            break;
+
+    
+
+
+        default:
+            break;
+    }
+}
+sayHello
