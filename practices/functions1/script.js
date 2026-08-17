e73adfbb36c1322ea9bfb3e4de51c255e42c1256
iitzkushal <iitzkushal@gmail.com>
2026-08-17 20:19:21 +0545

    use of function in different ways

--- practices/functions1/script.js
+++ practices/functions1/script.js
@@ -15,26 +15,28 @@ var greetings = namaste();
 console.log(greetings);
 console.log(namaste());
 
-function getUserRole(name,role){
+ var  getUserRole = function (name,role){
     switch (role){
         case "admin":
-        return`${name}is admin with all access` ;   
+        return`${name} is admin with all access` ;   
             break;//this is not necessary cause it has no role forr this
         case "subadmin":
             return`${name} is sub-admin with  access to create and delete cources` ;   
             break;
         case "testprepe":
             return `${name} is a test prep with access to create and delete tests`;
-    break;
-     case "user":
-        return `${name}is a user to consume content`;
+         break;
+         case "user":
+        return `${name} is a user to consume content`;
         break;
-        
 
-
-
+        case"trial user":
+        return`${name} is a trial user`;
         default:
             break;
     }
 }
-sayHello
+console.log(getUserRole("kushal","admin"));
+//different ways of using this shit named function
+var getRole = getUserRole("thapa","user");
+console.log(getRole);
