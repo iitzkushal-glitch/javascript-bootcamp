40c7dfe4eee9a0c8f36ffcffc373b827f5fc6349
iitzkushal <iitzkushal@gmail.com>
2026-08-21 18:21:37 +0545

    added new file for javascript

---
+++ javascript.course/script.js
@@ -0,0 +1,9 @@
+console.log("yo")
+
+var number = 2; 
+
+function tip(t){
+    var bill = t;
+    console.log(bill + 5);
+}
+tip(44) 
---
+++ javascript.course/scripts.js
@@ -0,0 +1,10 @@
+var number = 2; 
+
+function sayMe(){
+    console.log("kushal");
+}
+
+sayMe()
+
+
+console.log("whats up")
