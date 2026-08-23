1b2013bcfe6b7e6ced4069a1e028d95e0002bddd
iitzkushal <iitzkushal@gmail.com>
2026-08-23 08:47:05 +0545

    greeting bot

--- javascript.course/script.js
+++ javascript.course/script.js
@@ -1,10 +1,25 @@
 console.log("yo")
 
-var number = 2; 
+let number = 2; 
 
 function tip(t){
-    var bill = t;
+ let bill = t;
     console.log(bill + 5);
 }
 tip(44) 
--
+
+//greating bot
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
