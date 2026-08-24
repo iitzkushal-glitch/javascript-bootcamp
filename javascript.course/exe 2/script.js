ee9983cf2d218cc50672e936783f3d7846920a2e
iitzkushal <iitzkushal@gmail.com>
2026-08-24 15:23:32 +0545

    trivia bot

---
+++ javascript.course/exe 2/script.js
@@ -0,0 +1,16 @@
+console.log("yoo what's up");
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName ="kushal";
+let botLocation ="kathmandu";
+let favoriteLanguage ="python";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is a simple creature which swallows any creature it can fit on mouth";
+console.log(codingFact);
+
+codingFact = favoriteLanguage +" is an open-source, interpreted, high-level programming language created by Guido van Rossum";
+
+console.log(codingFact);
