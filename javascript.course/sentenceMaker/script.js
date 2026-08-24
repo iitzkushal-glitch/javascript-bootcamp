16ce3f84cea95ebf3b673ab7423f76b142c36222
iitzkushal <iitzkushal@gmail.com>
2026-08-24 18:09:42 +0545

    first story

--- javascript.course/sentenceMaker/script.js
+++ javascript.course/sentenceMaker/script.js
@@ -1,1 +1,20 @@
 console.log("heloooo")
+
+
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "scary";
+noun = "bot";
+verb = "running";
+place = "datacenter";
+adjective2 ="angry";
+noun2 ="knife";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
