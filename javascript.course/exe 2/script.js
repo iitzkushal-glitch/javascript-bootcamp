780f1d4c95bffd392880504d6a4ac36a4b90e247
iitzkushal <iitzkushal@gmail.com>
2026-08-24 15:34:31 +0545

    trivia bot final updates

--- javascript.course/exe 2/script.js
+++ javascript.course/exe 2/script.js
@@ -5,12 +5,22 @@ console.log("Hello! I'm your coding fun fact guide!");
 let botName ="kushal";
 let botLocation ="kathmandu";
 let favoriteLanguage ="python";
-console.log("My name is " + botName + " and I live on " + botLocation + ".");
-console.log("My favorite programming language is " + favoriteLanguage + ".");
+console.log("My name is " + botName + " and I live on " + botLocation 
+    + ".");
+console.log("My favorite programming language is " + favoriteLanguage 
+    + ".");
 
-let codingFact = favoriteLanguage + " is a simple creature which swallows any creature it can fit on mouth";
+let codingFact = favoriteLanguage +
+ " is a simple creature which swallows any creature it can fit on mouth";
 console.log(codingFact);
 
-codingFact = favoriteLanguage +" is an open-source, interpreted, high-level programming language created by Guido van Rossum";
+codingFact = favoriteLanguage +
+" is an open-source, interpreted, high-level programming language created by Guido van Rossum";
 
 console.log(codingFact);
+
+codingFact = "Final fact about " + favoriteLanguage + 
+" is that " + favoriteLanguage + " is older than java.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " 
+    + botName + " from " + botLocation + ".");
